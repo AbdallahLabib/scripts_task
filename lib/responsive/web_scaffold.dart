52f04c78c30e88a4de57f23eacdeb21046b79b1e
AbdullahLabib22 <abdolabib56@gmail.com>
2022-08-11 15:25:08 +0200

    remove prints

--- lib/responsive/web_scaffold.dart
+++ lib/responsive/web_scaffold.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:scripts_task/shared/assets/assets.gen.dart';
 
 class WebScaffold extends StatelessWidget {
@@ -24,7 +23,7 @@ class WebScaffold extends StatelessWidget {
 
                 //dashboard
                 Assets.icons.dashboard.svg(),
-                Text('Dashboard', style: TextStyle(),)
+                const Text('Dashboard', style: TextStyle(),)
 
 
               ],
