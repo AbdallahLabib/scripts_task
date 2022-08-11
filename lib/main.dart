8bf171dfeb30136ea627b167faf573b8f55dc173
AbdullahLabib22 <abdolabib56@gmail.com>
2022-08-11 15:15:01 +0200

    setup bloc observer and app routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scripts_task/responsive/mobile_scaffold.dart';
 import 'package:scripts_task/responsive/responsive_layout.dart';
 import 'package:scripts_task/responsive/tablet_scaffold.dart';
 import 'package:scripts_task/responsive/web_scaffold.dart';
+import 'package:scripts_task/shared/general/bloc_observer.dart';
+import 'package:scripts_task/shared/general/routes.dart';
+import 'package:scripts_task/shared/style/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runZonedGuarded(() {
+    Bloc.observer = AppBlocObserver();
+  }, (error, stackTrace) {
+    //log(error.toString(), stackTrace: stackTrace);
+    //FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final AppRouter appRouter;
+  const MyApp({
+    Key? key,
+    required this.appRouter,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Beetech',
+      theme: appTheme(),
+      onGenerateRoute: appRouter.onGenerateRoute,
       home: const ResponsiveLayout(
         mobileScaffold: MobileScaffold(),
         tabletScaffold: TabletScaffold(),
---
+++ lib/shared/general/routes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? onGenerateRoute(RouteSettings routeSettings) {}
+}
