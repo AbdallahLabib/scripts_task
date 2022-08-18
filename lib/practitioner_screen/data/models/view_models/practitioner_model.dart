485abe52b58aec91736645fb83911cf9da16148b
AbdullahLabib22 <abdolabib56@gmail.com>
2022-08-18 20:29:39 +0200

    models

---
+++ lib/practitioner_screen/data/models/view_models/practitioner_model.dart
@@ -0,0 +1,11 @@
+class Practitoner {
+  int id;
+  String name;
+  String imagePath;
+
+  Practitoner({
+    required this.id,
+    required this.name,
+    required this.imagePath,
+  });
+}
---
+++ lib/practitioner_screen/data/models/view_models/selections_model.dart
@@ -0,0 +1,11 @@
+class Selections {
+  int id;
+  String label;
+  String icon;
+
+  Selections({
+    required this.id,
+    required this.label,
+    this.icon = "",
+  });
+}
