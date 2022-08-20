6720053ccc46c8e6d3e19874febaecb2ee87f0b9
AbdullahLabib22 <abdolabib56@gmail.com>
2022-08-20 06:40:02 +0200

    add screen utils & localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:scripts_task/practitioner_screen/cubit/practitioner/practitioner_cubit.dart';
 import 'package:scripts_task/practitioner_screen/cubit/settings/settings_cubit.dart';
 import 'package:scripts_task/responsive/web_scaffold.dart';
@@ -9,8 +12,11 @@ import 'package:scripts_task/shared/general/bloc_observer.dart';
 import 'package:scripts_task/shared/general/routes.dart';
 import 'package:scripts_task/shared/style/app_theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  //EasyLocalization
+  await EasyLocalization.ensureInitialized();
 
   runApp(MyApp(appRouter: AppRouter()));
 
@@ -31,18 +37,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (_) => SettingsCubit()),
-        BlocProvider(create: (_) => PractitionerCubit()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Scripts',
-        theme: appTheme(),
-        onGenerateRoute: appRouter.onGenerateRoute,
-        home: const WebScaffold(),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(360, 773),
+      builder: (context, child) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => SettingsCubit()),
+            BlocProvider(create: (_) => PractitionerCubit()),
+          ],
+          child: EasyLocalization(
+            supportedLocales: const [
+              //Locale('en'),
+              Locale('ar'),
+            ],
+            useOnlyLangCode: true,
+            path: 'assets/lang',
+            fallbackLocale: const Locale('ar'),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Scripts',
+              theme: appTheme(),
+              onGenerateRoute: appRouter.onGenerateRoute,
+              home: child,
+              locale: context.locale,
+              supportedLocales: context.supportedLocales,
+              localizationsDelegates: [
+                FormBuilderLocalizations.delegate,
+                ...context.localizationDelegates,
+              ],
+            ),
+          ),
+        );
+      },
+      child: const WebScaffold(),
     );
   }
 }
